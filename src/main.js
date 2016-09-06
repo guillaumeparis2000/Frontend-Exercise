d9f03609e7a8a7af5becc71b5b956117b8042753
Guillaume PARIS <guillaume.paris.work@gmail.com>
2016-09-06 19:50:09 +0200

    Toggle visibility of dd elements when click on a dt element (accordion list)

--- src/main.js
+++ src/main.js
@@ -10,12 +10,42 @@ DocReady(() => {
 
     function activate() {
         hideAllDd();
+        addClickEventOnDt();
     }
 
     function hideAllDd() {
         const ddElements = document.getElementsByTagName('dd');
         for (const element of ddElements) {
             element.classList.add('u-hidden');
+        }
+    }
+
+    function addClickEventOnDt() {
+        const dtElements = document.getElementsByTagName('dt');
+
+        for (const element of dtElements) {
+            element.addEventListener('click', dtClick, false);
+        }
+    }
+
+    function dtClick() {
+        toggleDt(this);
+    }
+
+    function toggleDt(element) {
+        let hide = true;
+
+        if (element.nextElementSibling.classList.contains('u-hidden')) {
+            hide = false;
+        }
+
+        hideAllDd();
+
+        if (hide) {
+            element.nextElementSibling.classList.add('u-hidden')
+        }
+        else{
+            element.nextElementSibling.classList.remove('u-hidden')
         }
     }
 });
